f22aca9477ab9bf972171c3ee55c96574e2d40e5
aarkitekkt <johnson.brycej@gmail.com>
2019-11-30 08:48:14 -0700

    Added while loops to check prompts

--- java.js
+++ java.js
@@ -1,15 +1,3 @@
-var length = prompt("Enter password length(8-128)");
-
-if (length < 8) {
-    alert("Must be at least 8 characters");
-} else if (length > 128) {
-    alert("Must be less than 128 characters");
-}
-
-var numbersConfirm = confirm("Do you want to include numbers?");
-var specialConfirm = confirm("Do you want to include special characters?");
-var upperConfirm = confirm("Do you want to include uppercase letters?");
-var lowerConfirm = confirm("Do you want to include lowercase letters?");
 var characters = "";
 var charOptions = {
     numbers: "0123456789",
@@ -17,31 +5,48 @@ var charOptions = {
     lower: "abcdefghijklmnopqrstuvwxyz",
     upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 }
+//Prompt asking for desired password length
+var length = prompt("Enter password length(8-128)");
 
+//Check to see if password length is is the correct range.
+while (length < 8 || length > 128) {
+    alert("Password must be in between 8 and 128 characters");
+    var length = prompt("Enter password length(8-128)");
+}
+//Prompts to find out which types of characters to be included in password.
+var numbersConfirm = confirm("Do you want to include numbers?");
+var specialConfirm = confirm("Do you want to include special characters?");
+var upperConfirm = confirm("Do you want to include uppercase letters?");
+var lowerConfirm = confirm("Do you want to include lowercase letters?");
+
+//Check to see if at least one character type is selected for password.
+while (numbersConfirm === false && specialConfirm === false && upperConfirm === false && lowerConfirm === false) {
+    alert("You must choose at least one option");
+    var numbersConfirm = confirm("Do you want to include numbers?");
+    var specialConfirm = confirm("Do you want to include special characters?");
+    var upperConfirm = confirm("Do you want to include uppercase letters?");
+    var lowerConfirm = confirm("Do you want to include lowercase letters?");
+}
+
+//Combine all selected character types into single string.
 if (numbersConfirm === true) {
     characters += charOptions.numbers;
 }
-
 if (specialConfirm === true) {
     characters += charOptions.special;
 }
-
 if (upperConfirm === true) {
     characters += charOptions.upper;
 }
-
 if (lowerConfirm === true) {
     characters += charOptions.lower;
 }
 
-if (numbersConfirm === false && specialConfirm === false && upperConfirm === false && lowerConfirm === false) {
-    alert("You must choose at least one option!")
-}
 
 console.log(generator(length, characters))
 
 
-
+//Function to generate password given the length and character inputs.
 function generator(l, x) {
     var password = "";
     for (i = 0; i < l; i++) {
