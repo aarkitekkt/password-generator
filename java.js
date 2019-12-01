41e0e95f534e250b902572eabc43a6a1cc74e140
aarkitekkt <johnson.brycej@gmail.com>
2019-12-01 11:27:46 -0700

    add copy button functionality

--- java.js
+++ java.js
@@ -10,6 +10,7 @@ while (isNaN(length) === true) {
     alert("Must enter a number");
     var length = prompt("Enter password length(8-128)");
 }
+
 //Prompts to find out which types of characters to be included in password.
 var numbersConfirm = confirm("Do you want to include numbers?");
 var specialConfirm = confirm("Do you want to include special characters?");
@@ -47,15 +48,13 @@ if (lowerConfirm === true) {
     characters += charOptions.lower;
 }
 
-//when "Generate" button is clicked, run the function to generate the password and fill text box with password.
+//when "Generate" button is clicked, run the functions to generate the password and fill text box with password.
 document.getElementById("generate").onclick = function () { passFill() };
 
 function passFill() {
     var password = generator(length, characters);
     document.getElementById("passwordBox").value = password;
 }
-
-//Function to generate password given the length and character inputs.
 function generator(l, c) {
     var pw = "";
     for (i = 0; i < l; i++) {
@@ -64,6 +63,16 @@ function generator(l, c) {
     return pw;
 }
 
+//When "Copy" button is clicked, run the functions to copy generated password to clipboard.
+document.getElementById("copy").onclick = function () { copyPass() };
+
+function copyPass() {
+    var passCopy = document.getElementById("passwordBox");
+    passCopy.select();
+    passCopy.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Password copied to clipboard");
+}
 
 
 
